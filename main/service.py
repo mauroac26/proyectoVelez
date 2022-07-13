d43ca9e57804065821244cdb939dbb7bb7f775b9
mauroac26 <mauroac26@gmail.com>
2022-07-13 12:45:57 -0300

    cambios requeriment

--- main/service.py
+++ main/service.py
@@ -1,9 +1,7 @@
 #from datetime import datetime
-from re import T
+
 import requests
-import pandas as pd
-from bs4 import BeautifulSoup
-import json
+
 import datetime
 
 
@@ -13,16 +11,7 @@ def generate_request(url, params={}):
     if response.status_code == 200:
         return response.json()
 
-# def get_username(params={}):
-#     response = generate_request('https://serpapi.com/searches/f2f2618f7a907b4d/622faa3ac47d3c6f6b3416bc.json', params)
-    
-#     if response:
 
-#        equipos = response['knowledge_graph']['jugadores']
-    
-#        return equipos
-    
-#     return ''
 
 def get_username(params={}):
     
@@ -80,18 +69,6 @@ def jugador(response, posicion):
     
 
 
-# def resultados(params={}):
-#     response = generate_request('https://serpapi.com/search.json?engine=google&q=resultados+velez+sarsfield&location=Mexico&google_domain=google.com.mx&gl=mx&hl=es&api_key=380a1822a2d4935cffd76013b3b82740ee1ae43ee801bc0690edf4d5b916c0c5', params)
-    
-#     if response:
-
-#        fixture = response['sports_results']['games']
-       
-#        return fixture
-    
-#     return ''
-
-
 def resultados(params={}):
     response = generate_request('https://site.web.api.espn.com/apis/site/v2/sports/soccer/all/teams/21/schedule?region=ar&lang=es&season=2022', params)
     
@@ -125,9 +102,7 @@ def resultados(params={}):
 
                     resultado.append(tablaProsiciones)
                         
-                        # i +=1
-                        # if i == 6:
-                        #     break
+                     
          
         return resultado
     
@@ -170,91 +145,7 @@ def calendario(params={}):
     return ''
 
 
-# def scrap():
-#     url = 'https://argentina.as.com/resultados/futbol/copa_liga_argentina/clasificacion/'
 
-#     page = requests.get(url)
-
-#     soup = BeautifulSoup(page.content, 'html.parser')
-
-#     #Equipos
-#     eq = soup.find_all('span', class_='nombre-equipo')
-#     pt = soup.find_all('td', class_='destacado')
-    
-#     url_velez = 'https://velez.com.ar/futbol/estadisticas'
-  
-#     url_link = requests.get(url_velez) 
-#     file = BeautifulSoup(url_link.content, 'html.parser') 
-  
-#     find_table = file.find('table', class_='table table-bordered table-hover text-center') 
-#     rows = find_table.find_all('tr') 
-#     est = list()
-#     count = 0
-#     for i in rows: 
-#         table_data = i.find_all('td')
-#         data = [j.text for j in table_data] 
-#         est.append(data)
-
-#     posiciones = list()
-    
-#     count = 14
-#     i = 1
-#     while count < 28:
-        
-#         tablaProsiciones = {}
-#         tablaProsiciones['puesto'] = i
-#         tablaProsiciones['nombre'] = eq[count].text
-#         tablaProsiciones['puntos'] = pt[count].text
-#         tablaProsiciones['pj'] = est[i][2]
-#         tablaProsiciones['g'] = est[i][3]
-#         tablaProsiciones['e'] = est[i][4]
-#         tablaProsiciones['p'] = est[i][5]
-#         tablaProsiciones['gf'] = est[i][6]
-#         tablaProsiciones['gc'] = est[i][7]
-#         tablaProsiciones['dif'] = est[i][8]
-#         posiciones.append(tablaProsiciones)
-#         i +=1
-#         count += 1
-
-#     return posiciones
-
-# def prueba():
-#     URL = 'https://velez.com.ar/futbol/estadisticas'
-  
-#     url_link = requests.get(URL) 
-#     file = BeautifulSoup(url_link.content, 'html.parser') 
-  
-#     find_table = file.find('table', class_='table table-bordered table-hover text-center') 
-#     rows = find_table.find_all('tr') 
-#     est = list()
-    
-#     for i in rows: 
-#         table_data = i.find_all('td')
-#         data = [j.text for j in table_data] 
-#         est.append(data)
-#         # for j in table_data:
-#         #     est.append(j.text)
-
-
-#     datos = list()  
-    
-#     i = 1
-#     while i < 15:
-            
-#         signer_json = {}
-       
-#         signer_json['pts'] = est[i][1]
-#         signer_json['pj'] = est[i][2]
-#         signer_json['g'] = est[i][3]
-#         signer_json['e'] = est[i][4]
-#         signer_json['p'] = est[i][5]
-#         signer_json['gf'] = est[i][6]
-#         signer_json['gc'] = est[i][7]
-#         signer_json['dif'] = est[i][8]
-#         datos.append(signer_json)
-#         i += 1
-
-#     return datos
   
 def tabla(params={}):
     response = generate_request('https://site.web.api.espn.com/apis/v2/sports/soccer/ARG.1/standings?region=ar&lang=es&contentorigin=deportes&season=2022&sort=rank%3Aasc', params)
