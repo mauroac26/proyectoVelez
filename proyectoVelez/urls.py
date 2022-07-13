fdea8978aa3ac93c95c71e034ff726197e9d4f18
mauroac26 <mauroac26@gmail.com>
2022-07-13 13:08:17 -0300

    cambios generales

--- proyectoVelez/urls.py
+++ proyectoVelez/urls.py
@@ -23,4 +23,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+] 
