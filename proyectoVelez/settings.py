97973266f0b55810ca7c06769f12139893618900
mauroac26 <mauroac26@gmail.com>
2022-07-13 12:40:42 -0300

    archivos heroku

--- proyectoVelez/settings.py
+++ proyectoVelez/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'proyectoVelez.urls'
@@ -132,3 +133,5 @@ os.makedirs(STATIC_ROOT, exist_ok=True)
 STATICFILES_DIR = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
